3cfd8fc87c9da1097fafbe81411de02c1483a129
taleetarhm <124320571+taleetarhm@users.noreply.github.com>
2023-02-13 11:46:20 +0700

    Create home.php

---
+++ admin/home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>NAVBAR</title>
+	<link rel="stylesheet" type="text/css" href="stylehome.css">
+</head>
+<body>
+
+	<nav>
+		<div class="wrapper">
+			<div class="logo"><a href='SNC'>SNC</div>
+			<div class="menu">
+				<ul>
+					<li><a href="v_form.php">Daftar</a></li>
+					<li><a href="dataevent.php">Data Event</a></li>
+					<li><a href="web/info.php"  class="tbl-biru">Sign Out</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<br>
+    <div class="hero">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="texthero">
+                	<center>
+                		<img src="yellow-green-pink-streamers-brown-background.jpg" width="1400" height="600" >
+                	<h1>WELCOME TO</h1>
+                   <h1>Semarang Night Carnival</h1>
+               </center>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
